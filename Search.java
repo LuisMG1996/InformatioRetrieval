e638e3662fd3ed0c26f9953b1ea278284cac5db9
luisricardo <luis.montesgz@udlap.mx>
2018-08-17 14:25:59 -0500

    Second commit

---
+++ Search.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by luisricardo on 17/08/2018.
+ */
+public class Search {
+    
+}
--- TextFile.java
+++ TextFile.java
@@ -1,9 +1,23 @@
 package com.company;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
 /**
  * Created by luisricardo on 17/08/2018.
  */
-public class TextFile extends File {
+public class TextFile {
+    String path;
+    String nameFile;
+    File txtFile;
+
+    public TextFile(String path, String nameFile){
+        try {
+            txtFile = new File(path);
+            BufferedReader br = new BufferedReader(new FileReader(txtFile));
+        }catch (Exception e){
+            System.out.println("Error loading file: " + e);
+        }
+    }
 }
